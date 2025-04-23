0e04a800a7ab8d188f1ec04a6cfde8e6d6ea1d88
HuskyFawkes <32594348+HuskyFawkes@users.noreply.github.com>
2025-04-23 10:52:28 -0300

    23-04
    Removing unacessary blog posts pages and leaving the one hand made

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -245,7 +245,7 @@ const about = {
 
 const blog = {
   label: "Blog",
-  title: "Writing about design and tech...",
+  title: "My hub for project documentation and maybe some random stuff",
   description: `Read what ${person.name} has been up to recently`,
   // Create new blog posts by adding a new .mdx file to app/blog/posts
   // All posts will be listed on the /blog route
